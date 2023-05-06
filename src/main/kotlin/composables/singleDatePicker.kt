6ce16ecedbae0d47ab69ebb5249b352f12f2f928
gleb-skobinsky <glebgytnik@gmail.com>
2023-05-06 16:49:47 +0300

    Code refactor

--- src/main/kotlin/composables/singleDatePicker.kt
+++ src/main/kotlin/composables/singleDatePicker.kt
@@ -3,7 +3,6 @@ package composables
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import dateTypes.DateTypes
